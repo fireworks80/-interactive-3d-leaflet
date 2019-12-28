ef7f02b1262efa6ed37c83f92a90bc8371c514c8
fireworks80 <fireworks80@naver.com>
2019-12-28 22:51:22 +0900

    전체 리플릿 닫기

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 (() => {
   const leafletEl = document.querySelector('.leaflet');
+  const pageEls = document.querySelectorAll('.page');
 
   const getDelegationEventEl = (target, className) => {
     while (!target.classList.contains(className)) {
@@ -13,11 +14,21 @@
     return target;
   };
 
+  const closeLeaflet = () => {
+    pageEls[2].classList.remove('is-fliped');
+    setTimeout(() => {
+      pageEls[0].classList.remove('is-fliped');
+    }, 500);
+  };
+
   leafletEl.addEventListener('click', e => {
     const eventEl = getDelegationEventEl(e.target, 'page');
 
     if (!eventEl) return;
-
     eventEl.classList.add('is-fliped');
+
+    // close all
+    const closeAllBtn = getDelegationEventEl(e.target, 'page__close');
+    if (closeAllBtn) closeLeaflet();
   });
 })();
